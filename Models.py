a147b5fd181d9e610997f41eb4aaaa3b9740cd38
Kerr206011 <138211437+Kerr206011@users.noreply.github.com>
2024-04-15 14:48:47 +0200

    Update Models.py

--- Models.py
+++ Models.py
@@ -1,5 +1,7 @@
 import pygame
 
 class Model():
-    def __init__(self) -> None:
-        self.Ap = 
+    def __init__(self, AP, image) -> None:
+        self.Ap = AP
+        self.image = image
+        self.face = (1,0)
