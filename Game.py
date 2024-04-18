b343ecc5a329bd05c7ec57664a592211aaa6a57b
Kerr206011 <138211437+Kerr206011@users.noreply.github.com>
2024-04-18 19:15:56 +0200

    Update

--- Game.py
+++ Game.py
@@ -1,3 +1,22 @@
+import json
+import random
 
 class Game():
-    pass
+    def __init__(self) -> None:
+        self.map = []
+        self.blipList = ()
+        self.cp = int
+        self.player1 = "";self.player2 = ""
+        self.gsModelList = [];self.smModelList = [];self.blipList = []
+        self.level = int
+
+    def load_level(self,levelFile):
+        file_path = levelFile+".json"
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+        self.level = data["level"]
+        self.blipList = data["bliplist"]
+        
+game = Game()
+game.load_level("data")
+print(game.level)
--- Speichereinheit.py
+++ Speichereinheit.py
@@ -1,9 +1,30 @@
 import json
 
-x = {"name" : "tom",
-     "Zug" : 1,
-     "Regler" : 0.125}
+# Data to be written to the JSON file
+a = []
+i = 0
+while i < 9:
+    a.append(1)
+    a.append(3)
+    i+=1
+i = 0
+while i<4:
+    a.append(2)
+    i+= 1
+data = {
+    "level": 1,
+    "blipList" : a
+}
 
-y = json.dumps(x)
+# File path to save the JSON file
+file_path = "level1.json"
 
-print(y)
+# Writing data to the JSON file
+with open(file_path, 'w') as json_file:
+    json.dump(data, json_file, indent=4)
+
+with open(file_path, 'r') as json_file:
+    data = json.load(json_file)
+
+# Printing loaded data
+print(data)
